830fc7f4ab59eccdd4f3bca668e729a1f401de9e
吉見菜那 <93458998+YoshimiNana@users.noreply.github.com>
2023-08-23 08:42:13 +0000

    LINEボットの作成完了

--- functions/src/utils/env.ts
+++ functions/src/utils/env.ts
@@ -1,3 +1,5 @@
 export const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID ?? '';
 export const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET ?? '';
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? '';
+export const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN ?? '';
+export const CHANNEL_SECRET = process.env.CHANNEL_SECRET ?? '';
