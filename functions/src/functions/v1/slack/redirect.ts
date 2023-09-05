024400c274bb0ee69b7bc2e704dd44dce07a4f0c
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2023-09-05 23:06:04 +0000

    Slack Bot の追加後にそのままアカウントの認証に飛ぶ

--- functions/src/functions/v1/slack/redirect.ts
+++ functions/src/functions/v1/slack/redirect.ts
@@ -1,5 +1,11 @@
-import { setSlackToken } from "../../../utils/firestore/slack-token";
+import { FIREBASE_HOSTING_URL } from "../../../utils/env";
+import {
+  getVerifiedSlackUser,
+  setSlackToken
+} from "../../../utils/firestore/slack-token";
+import { createSlackVerificationCode } from "../../../utils/firestore/slack-verification-code";
 import { functions128MB } from "../../../utils/functions";
+import { generateUrlWithParams } from "../../../utils/generate-url-with-parans";
 import { fetchSlackAccessToken } from "../../../utils/slack/fetch-access-token";
 
 export const slackRedirect = functions128MB.https.onRequest(
@@ -19,13 +25,15 @@ export const slackRedirect = functions128MB.https.onRequest(
       const refreshToken = data.refresh_token;
       const botUserId = data.bot_user_id;
       const teamId = data.team?.id;
+      const slackUserId = data.authed_user?.id;
 
       if (
         !accessToken ||
         !expiresInSeconds ||
         !refreshToken ||
         !botUserId ||
-        !teamId
+        !teamId ||
+        !slackUserId
       ) {
         res.status(500).send("Error: Missing data from Slack API");
         return;
@@ -39,10 +47,24 @@ export const slackRedirect = functions128MB.https.onRequest(
         botUserId
       });
 
-      res.status(200).redirect(`https://app.slack.com/client/${teamId}`);
+      const user = await getVerifiedSlackUser(teamId, slackUserId);
+      if (user) {
+        res.status(200).redirect(`https://app.slack.com/client/${teamId}`);
+        return;
+      }
+
+      const verificationCode = await createSlackVerificationCode({
+        slackUserId,
+        slackTeamId: teamId
+      });
+      const verificationUrl = generateUrlWithParams(FIREBASE_HOSTING_URL, {
+        code: verificationCode.code
+      });
+
+      res.status(200).redirect(verificationUrl);
     } catch (error) {
       console.log(error);
       res.status(500).send("Internal Server Error");
     }
-  },
+  }
 );
