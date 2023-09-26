12f7659c4bf6f4a374f4236ebfaa5c292b77e87f
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2023-09-26 01:51:43 +0000

    setup production env

--- hosting/src/utils/env.ts
+++ hosting/src/utils/env.ts
@@ -1,2 +1,8 @@
-// TODO: Branch dev and prod
-export const APP_DYNAMIC_LINK = "https://kamachokkai.page.link/sora-dev";
+// Vite modes
+// See: https://vitejs.dev/guide/env-and-mode.html
+const mode = import.meta.env.MODE;
+
+export const APP_DYNAMIC_LINK =
+  mode === "prod"
+    ? "https://sorakamachokkai.page.link/sora"
+    : "https://kamachokkai.page.link/sora-dev";
