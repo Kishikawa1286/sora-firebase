a1209940cf5a53945922f846c7beb2e39ff121ca
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2023-10-01 12:55:58 +0000

    Firebase Hosting の URL 切り替え

--- functions/src/utils/env.ts
+++ functions/src/utils/env.ts
@@ -1,8 +1,10 @@
+// Secrets
 export const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID ?? "";
 export const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET ?? "";
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "";
 export const LINE_CHANNEL_ACCESS_TOKEN =
   process.env.LINE_CHANNEL_ACCESS_TOKEN ?? "";
 export const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET ?? "";
-// TODO: Change this depending on the environment
-export const FIREBASE_HOSTING_URL = "https://sora-dev-kam.web.app";
+
+// .env.*
+export const FIREBASE_HOSTING_URL = process.env.FIREBASE_HOSTING_URL ?? "";
