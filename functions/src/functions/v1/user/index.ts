8a4c092750e37c8e7a0aff97e04dac11c47c30e2
Kentaro Ishikawa <53816975+Kishikawa1286@users.noreply.github.com>
2023-10-01 09:55:14 +0000

    setScheduleAdjustmentUrl の export

--- functions/src/functions/v1/user/index.ts
+++ functions/src/functions/v1/user/index.ts
@@ -1,2 +1,3 @@
 export { onCreateUser as on_create } from "./on-create";
 export { onDeleteUser as on_delete } from "./on-delete";
+export { setScheduleAdjustmentUrl as set_schedule_adjustment_url } from "./set-schedule-adjustment-url";
